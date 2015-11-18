16d788bd28024722bf79e92e155069c811f923b4
Mike Easley <mike.easley@gmail.com>
2015-11-18 10:56:14 -0700

    Updating the styling on the dots

--- examples/example-hero.js
+++ examples/example-hero.js
@@ -1,4 +1,4 @@
-var heroList = ['Grumpy Cat', 'Hover Cat', 'Bread Cat', 'Business Cat', 'Hipster Cat', 'Lime Cat', 'Mellow Cat'];
+var heroList = ['Grumpy Cat', 'Hover Cat', 'Bread Cat', 'Business Cat', 'Hipster Cat', 'Lime Cat', 'Mellow Cat', 'Rainbow Cat', 'Dreamer Cat', 'Secret Cat', 'Turbo Cat', 'Ultra Cat', 'Fancy Cat', 'Dancy Cat', 'Prancy Cat'];
 
 ReactDOM.render(
 	React.createElement(
