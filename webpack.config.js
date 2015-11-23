7f5ea660ebe597ca27d179b62f1ac7bfd853c893
Admiral Potato <admiral@nuclearpixel.com>
2015-11-23 11:13:59 -0700

    Adjusting webpack.config.js so lib output is external to the examples

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,70 @@
 var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
-	entry: {
-		'lib/react-slide-select': './src/react-slide-select',
-		'examples/examples': [
-			'./examples/example-custom_children',
-			'./examples/example-product',
-			'./examples/example-hero'
-		]
-	},
-	output: {
-		library: 'SlideSelect',
-		libraryTarget: 'umd',
-		path: path.resolve(__dirname),
-		filename: '[name].js'
-	},
-	devtool: 'source-map',
-	module: {
-		loaders: [
-			{
-				test: /\.jsx$/,
-				loader: 'babel',
-				exclude: /node_modules/,
-				query: {
-					presets: ['react', 'es2015']
-				}
+var moduleProps = {
+	loaders: [
+		{
+			test: /\.jsx$/,
+			loader: 'babel',
+			exclude: /node_modules/,
+			query: {
+				presets: ['react', 'es2015']
 			}
-		]
-	},
-	externals: [{
-		'react': {
-			root: 'React',
-			commonjs2: 'react',
-			commonjs: 'react',
-			amd: 'react'
-		},
-		'react-dom': {
-			root: 'ReactDOM',
-			commonjs2: 'react-dom',
-			commonjs: 'react-dom',
-			amd: 'react-dom'
 		}
-	}],
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	}
+	]
 };
+var resolveProps = {
+	extensions: ['', '.js', '.jsx']
+};
+
+module.exports = [
+	{
+		entry: {
+			'lib/react-slide-select': './src/react-slide-select'
+		},
+		output: {
+			library: 'SlideSelect',
+			libraryTarget: 'umd',
+			path: path.resolve(__dirname),
+			filename: '[name].js'
+		},
+		externals: [{
+			'react': {
+				root: 'React',
+				commonjs2: 'react',
+				commonjs: 'react',
+				amd: 'react'
+			},
+			'react-dom': {
+				root: 'ReactDOM',
+				commonjs2: 'react-dom',
+				commonjs: 'react-dom',
+				amd: 'react-dom'
+			}
+		}],
+		devtool: 'source-map',
+		module: moduleProps,
+		resolve: resolveProps
+	},
+	{
+		entry: {
+			'examples/examples': [
+				'./examples/example-custom_children',
+				'./examples/example-product',
+				'./examples/example-hero'
+			]
+		},
+		output: {
+			path: path.resolve(__dirname),
+			filename: '[name].js'
+		},
+		externals: [{
+			'react': 'React',
+			'react-dom': 'ReactDOM',
+			'../lib/react-slide-select': 'SlideSelect'
+		}],
+		devtool: 'source-map',
+		module: moduleProps,
+		resolve: resolveProps
+	}
+];
